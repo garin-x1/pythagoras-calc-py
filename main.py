64f02bdad6445415ed2b4a07896512762f5be9bf
struggler <garinjanu07@gmail.com>
2022-11-10 04:41:28 +0700

    little UI update

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from math import sqrt
 
 def get_a(b,hypotenuse):
     a_2 = (hypotenuse**2) - (b**2)
-    print(f"{hypotenuse**2} - {b**2} = {a_2} => {sqrt(a_2)}\n")
+    print(f"\n{hypotenuse**2} - {b**2} = {a_2} => {sqrt(a_2)}\n")
 
 def get_b(a, hypotenuse):
     b_2 = (hypotenuse**2) - (a**2)
-    print(f"{hypotenuse**2} - {a**2} = {b_2} => {sqrt(b_2)}\n")
+    print(f"\n{hypotenuse**2} - {a**2} = {b_2} => {sqrt(b_2)}\n")
 
 def get_hypotenuse(a,b):
     hypotenuse_2 = (a**2) + (b**2)
-    print(f"{a**2} + {b**2} = {hypotenuse_2} => {sqrt(hypotenuse_2)}\n")
+    print(f"\n{a**2} + {b**2} = {hypotenuse_2} => {sqrt(hypotenuse_2)}\n")
 
 while True:
     print("-------------------------------")
@@ -23,7 +23,7 @@ while True:
     c = input("c = ")
 
     if (a == "" and b == "") or (a == "" and c == "") or (b == "" and c == ""):
-        print("Please enter at least two of them\n")
+        print("\nPlease enter at least two of them\n")
     elif a == "":
         b = int(b)
         c = int(c)
@@ -37,4 +37,4 @@ while True:
         b = int(b)
         get_hypotenuse(a,b)
     else:
-        print("All variable have been filled\n")
+        print("\nAll variable have been filled\n")
