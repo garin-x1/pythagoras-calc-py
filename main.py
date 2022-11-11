01784aebe718697c5a79311599b7c50a498b2b7c
struggler <garinjanu07@gmail.com>
2022-11-11 21:49:06 +0700

    fix tipe float dan update UI

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from math import sqrt
 
 def cari_a(b,hipotenusa):
     a_2 = (hipotenusa**2) - (b**2)
-    print(f"\n{hipotenusa**2} - {b**2} = {a_2} => {sqrt(a_2)}\n")
+    print(f"\n=> {hipotenusa**2} - {b**2} = {a_2} => {sqrt(a_2)}\n")
 
 def cari_b(a, hipotenusa):
     b_2 = (hipotenusa**2) - (a**2)
-    print(f"\n{hipotenusa**2} - {a**2} = {b_2} => {sqrt(b_2)}\n")
+    print(f"\n=> {hipotenusa**2} - {a**2} = {b_2} => {sqrt(b_2)}\n")
 
 def cari_hipotenusa(a,b):
     hipotenusa_2 = (a**2) + (b**2)
-    print(f"\n{a**2} + {b**2} = {hipotenusa_2} => {sqrt(hipotenusa_2)}\n")
+    print(f"\n=> {a**2} + {b**2} = {hipotenusa_2} => {sqrt(hipotenusa_2)}\n")
 
 while True:
     print("-------------------------------")
@@ -23,18 +23,18 @@ while True:
     c = input("c = ")
 
     if (a == "" and b == "") or (a == "" and c == "") or (b == "" and c == ""):
-        print("\nTolong isi setidaknya dua dari itu\n")
+        print("\n=> Tolong isi setidaknya dua dari itu\n")
     elif a == "":
-        b = int(b)
-        c = int(c)
+        b = float(b)
+        c = float(c)
         cari_a(b,c)
     elif b == "":
-        a = int(a)
-        c = int(c)
+        a = float(a)
+        c = float(c)
         cari_b(a,c)
     elif c == "":
-        a = int(a)
-        b = int(b)
+        a = float(a)
+        b = float(b)
         cari_hipotenusa(a,b)
     else:
-        print("\nSemua variabel telah terisi\n")
+        print("\n=> Semua variabel telah terisi\n")
